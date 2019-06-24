166a20dd0ca375b7cb8dd2c46eef77adc0804063
Jamie Ev <jamie.osprey@gmail.com>
2019-06-23 20:34:34 -0500

    Add React/Redux dependencies and run build

--- src/app/index.js
+++ src/app/index.js
@@ -1,6 +1,6 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var ReactRedux = require('react-redux);
+var ReactRedux = require('react-redux');
 var Redux = require('redux');
 
 // Redux code
